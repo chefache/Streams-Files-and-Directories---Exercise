cd7bf81dbf12991bce8b3883d69e9db0cfdbfe30
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-18 22:57:26 +0200

    Add files via upload

---
+++ WarmUp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace WarmUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine(Environment.CurrentDirectory); //показва текучата директория на приложението;
+
+            StreamWriter writer = new StreamWriter("myTextFile.txt");
+            writer.Write("Hello world!");
+            writer.Close();
+        }
+    }
+}
