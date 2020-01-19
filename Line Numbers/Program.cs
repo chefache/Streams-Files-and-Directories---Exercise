820e1728df0047e08ff3a9e91e5dacfb0b634d69
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-19 22:44:22 +0200

    Add files via upload

---
+++ Line Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("input.txt"))
+            {
+                using (var writer = new StreamWriter("output.txt"))
+                {
+                    int lineCounter = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        writer.WriteLine($"{lineCounter} {line}");
+                        lineCounter++;
+                    }               
+                }
+            }
+        }
+    }
+}
