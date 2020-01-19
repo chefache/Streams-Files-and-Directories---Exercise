e50fb296309b05fb30e6cd9c27748d6f894868f2
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-19 22:44:00 +0200

    Add files via upload

---
+++ Odd Lines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            using (var reader = new StreamReader("input.txt"))
+            {
+                int counter = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (counter % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                        Console.WriteLine();
+                    }
+                    counter++;
+                }
+            }
+        }
+    }
+}
