d6ed277c6ca7b067d026c0625be4d504446b584d
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-20 19:56:46 +0200

    Add files via upload

---
+++ Slice a File/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            var totalSize = new FileInfo("Work File.txt").Length;
+            var sizePerFile = (int)Math.Ceiling(totalSize / 5.0);
+
+            using (var reader = new FileStream("Work File.txt", FileMode.Open))
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    var buffer = new byte[sizePerFile];
+                    var readBytes = reader.Read(buffer, 0, sizePerFile);
+
+                    using (var writer = new FileStream($"file-{i}.txt", FileMode.OpenOrCreate))
+                    {
+                        writer.Write(buffer, 0, readBytes);
+                    }
+                }
+            }           
+        }
+    }
+}
