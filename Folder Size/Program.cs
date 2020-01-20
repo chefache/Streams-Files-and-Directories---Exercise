5ceb324ad8c4b4d1bbd7dc1a1e18615dbc2985c1
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-20 19:57:26 +0200

    Add files via upload

---
+++ Folder Size/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(Environment.CurrentDirectory);
+            var totalLenght = 0L;
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                totalLenght += fileInfo.Length;
+            }
+            Console.WriteLine(totalLenght);
+        }
+    }
+}
