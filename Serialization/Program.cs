4bca554b4c7411b7f13923d447386fca1c307dac
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-21 18:15:45 +0200

    Add files via upload

---
+++ Serialization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int[] Grades { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student();
+
+            student.Name = "Stefan";
+            student.Age = 34;
+            student.Grades = new[] { 3, 4, 5, 3, 4 };
+
+            var formatter = new BinaryFormatter();
+            using (var stream = new FileStream("studentInfo.txt",FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                formatter.Serialize(stream, student);
+            }
+
+            using (var reader = new FileStream("studentInfo.txt", FileMode.OpenOrCreate))
+            {
+                var dataBuffer = new byte[300];
+
+                reader.Read(dataBuffer, 0, dataBuffer.Length);
+            }
+        }
+    }
+}
