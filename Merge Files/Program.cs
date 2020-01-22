c519140928681c9f356abe64ea2732fa7d2d3dda
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-22 18:23:41 +0200

    Add files via upload

---
+++ Merge Files/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            using (var reader = new StreamReader("input1.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    list.Add(int.Parse(line));
+                }
+            }
+            using (var reader = new StreamReader("input2.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    list.Add(int.Parse(line));
+                }
+            }
+            using (var writer = new StreamWriter("output.txt"))
+            {
+                foreach (var num in list.OrderBy(x => x))
+                {
+                    writer.WriteLine(num);
+                }
+            }
+        }
+    }
+}
